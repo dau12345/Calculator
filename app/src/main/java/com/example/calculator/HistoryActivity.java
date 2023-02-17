d5e4fd6e94f753574eae2a7628e661aa7a833015
Lồ Sìn Dậu <89368649+dau12345@users.noreply.github.com>
2023-02-17 19:39:59 +0700

    Complete Calculator + History Activity
    Complete Calculator:
    - Format big number to scientific notation
    - Limit digit of a number (max 9)
    - Save instances' state when rotate device screen
    - Use intent to pass array list to history activity
    History Activity:
    - Use intent to receive data from main activity
    - Create TextView programmatically to show all math solution
    - Save instances' state when rotate device screen

--- app/src/main/java/com/example/calculator/HistoryActivity.java
+++ app/src/main/java/com/example/calculator/HistoryActivity.java
@@ -2,13 +2,76 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
+
+    LinearLayout linearLayout;
+    Button btnClose;
+    ArrayList<ArrayList> solList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        linearLayout = findViewById(R.id.linearLayoutHistory);
+        btnClose = findViewById(R.id.btnClose);
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                finish();
+            }
+        });
+
+        //Receive data from main activity
+        Intent intent = getIntent();
+        solList = (ArrayList<ArrayList>) intent.getSerializableExtra("solList");
+
+        for (ArrayList arrTemp : solList) {
+            String str = arrTemp.get(0) + " " + arrTemp.get(1) + " " + arrTemp.get(2) + " = " + arrTemp.get(3);
+
+            // Create TextView programmatically.
+            TextView textView = new TextView(this);
+            textView.setText(str);
+            textView.setTextColor(getColor(R.color.white));
+            textView.setPadding(25, 0, 25, 0);
+            textView.setTextSize(40);
+            textView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT
+            );
+
+            textView.setLayoutParams(params);
+
+            // Add TextView to LinearLayout
+            if (linearLayout != null) {
+                linearLayout.addView(textView);
+            }
+        }
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putSerializable("solList", solList);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        super.onRestoreInstanceState(savedInstanceState);
+
+        solList = (ArrayList<ArrayList>) savedInstanceState.getSerializable("solList");
     }
 }
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -20,9 +23,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btnSeven, btnEight, btnNine, btnMultiply,
             btnFour, btnFive, btnSix, btnSubtract,
             btnOne, btnTwo, btnThree, btnPlus,
-            btnZero, btnComma, btnEqual;
+            btnZero, btnComma, btnEqual, btnHistory;
 
-    String finalResult = "";
+    String finalResult = "0";
 
     //pressedBtnId is used to store operation button's id which has been clicked
     int pressedBtnId = 0;
@@ -36,11 +39,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //newSol is set to "true" when the equal button has been clicked
     boolean newSolState = false;
 
-    ArrayList<ArrayList> solList = new ArrayList<>();
-
     BigDecimal currentNum = new BigDecimal("0");
     BigDecimal tempNum = new BigDecimal("0");
     BigDecimal resultNum = new BigDecimal("0");
+
+    ArrayList<ArrayList> solList = new ArrayList<ArrayList>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +51,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         tvResult = findViewById(R.id.result);
+        tvResult.setText("0");
+        currentNum = new BigDecimal(tvResult.getText().toString());
+
         assignBtn(btnAC, R.id.btnAC);
         assignBtn(btnPlus_or_Sub, R.id.btnPlus_or_Sub);
         assignBtn(btnPercent, R.id.btnPercent);
@@ -67,6 +73,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         assignBtn(btnZero, R.id.btnZero);
         assignBtn(btnComma, R.id.btnComma);
         assignBtn(btnEqual, R.id.btnEqual);
+        assignBtn(btnHistory, R.id.btnHistory);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean("clickState", clickState);
+        savedInstanceState.putBoolean("needCalcState", needCalcState);
+        savedInstanceState.putBoolean("newSolState", newSolState);
+        savedInstanceState.putInt("pressedBtnId", pressedBtnId);
+        savedInstanceState.putString("finalResult", finalResult);
+        savedInstanceState.putString("currentNum", currentNum.toString());
+        savedInstanceState.putString("tempNum", tempNum.toString());
+        savedInstanceState.putString("resultNum", resultNum.toString());
+        savedInstanceState.putString("tvResult", tvResult.getText().toString());
+        savedInstanceState.putSerializable("solList", solList);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        clickState = savedInstanceState.getBoolean("clickState");
+        needCalcState = savedInstanceState.getBoolean("needCalcState");
+        newSolState = savedInstanceState.getBoolean("newSolState");
+        finalResult = savedInstanceState.getString("finalResult");
+        currentNum = new BigDecimal(savedInstanceState.getString("currentNum"));
+        tempNum = new BigDecimal(savedInstanceState.getString("tempNum"));
+        resultNum = new BigDecimal(savedInstanceState.getString("resultNum"));
+        tvResult.setText(savedInstanceState.getString("tvResult"));
+        pressedBtnId = savedInstanceState.getInt("pressedBtnId");
+        solList = (ArrayList<ArrayList>) savedInstanceState.getSerializable("solList");
+        if (pressedBtnId > 0) {
+            changeBtnColor(findViewById(pressedBtnId));
+        }
     }
 
     //Identify buttons and set onclick listener for those buttons
@@ -76,21 +118,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     //Change operation button's bg color and text color, also save the pressed button's id
-    void createState(@NonNull Button btn) {
+    void changeBtnColor(@NonNull Button btn) {
         btn.setBackgroundColor(getColor(R.color.white));
         btn.setTextColor(Color.parseColor("#F39D3A"));
         pressedBtnId = btn.getId();
     }
 
     //Change operation button's bg color and text color back to default, also clear the pressed button's id
-    void disposeState(@NonNull Button btn) {
+    void disposeBtnColor(@NonNull Button btn) {
         btn.setBackgroundColor(Color.parseColor("#F39D3A"));
         btn.setTextColor(getColor(R.color.white));
         pressedBtnId = 0;
     }
 
-    void pressAction(Button btn) {
-        //If user has entered complete math problem but not clicked equal button yet -> calc
+    void pressButton(Button btn) {
+        //If user has entered complete math problem but not clicked equal button yet -> calculate it
         if (needCalcState) {
             calc();
             needCalcState = false;
@@ -98,48 +140,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //If pressed button's id is already existed -> dispose it
         if (pressedBtnId != 0) {
-
-            disposeState(findViewById(pressedBtnId));
+            disposeBtnColor(findViewById(pressedBtnId));
         }
 
-        createState(btn);
+        changeBtnColor(btn);
         tempNum = currentNum;
         clickState = true;
         newSolState = false;
     }
 
+    //Format big number to scientific notation
+    String formatSicNo(String bigNumber){
+        DecimalFormat df1 = new DecimalFormat("@######");
+        df1.setMinimumFractionDigits(0);
+        DecimalFormat df2 = new DecimalFormat("@#####E0");
+        df2.setMinimumFractionDigits(1);
+
+        if (bigNumber.length() <= 8) {
+            bigNumber = df1.format(new BigDecimal(bigNumber));
+        } else {
+            bigNumber = df2.format(new BigDecimal(bigNumber));
+        }
+
+        return bigNumber;
+    }
+
     void calc() {
-        //If user hasn't clicked any operation buttons yet -> break
+        //If there is no operation button has been clicked -> return
         if (pressedBtnId == 0) {
             return;
         }
 
         Button pressedBtn = findViewById(pressedBtnId);
-        switch (pressedBtn.getText().toString()) {
-            case "/":
-                resultNum = tempNum.divide(currentNum);
+        switch (pressedBtn.getId()) {
+            case R.id.btnDivide:
+                resultNum = tempNum.divide(currentNum, 2, RoundingMode.HALF_UP);
                 break;
-            case "X":
+            case R.id.btnMultiply:
                 resultNum = tempNum.multiply(currentNum);
                 break;
-            case "-":
+            case R.id.btnSubtract:
                 resultNum = tempNum.subtract(currentNum);
                 break;
-            case "+":
+            case R.id.btnPlus:
                 resultNum = tempNum.add(currentNum);
                 break;
             default:
                 break;
         }
 
-        //Dispose the pressed operation button when calculated successfully
-        disposeState(pressedBtn);
-
         //newSol is set to "true" to pronounce that program is ready for a new math problem
         newSolState = true;
 
-        //Display the result on screen
-        finalResult = resultNum.stripTrailingZeros() + "";
+        //Format big number to scientific notation
+        finalResult = formatSicNo(resultNum.stripTrailingZeros().toPlainString());
+        String tempNumString = formatSicNo(tempNum.toPlainString());
+        String currentNumString = formatSicNo(currentNum.toPlainString());
+
+        //Save history to solList array
+        ArrayList<Object> solution = new ArrayList<Object>();
+        solution.add(tempNumString);
+        Button btnTemp = findViewById(pressedBtnId);
+        solution.add(btnTemp.getText().toString());
+        solution.add(currentNumString);
+        solution.add(finalResult);
+
+        solList.add(solution);
+
+        //Display result on screen
         if (finalResult.length() > 0) {
             tvResult.setText(finalResult);
         }
@@ -147,8 +215,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Update the new currentNum
         currentNum = new BigDecimal(finalResult);
 
-        //Save history
-
+        //Dispose the pressed operation button when calculated successfully
+        disposeBtnColor(pressedBtn);
     }
 
     @Override
@@ -158,62 +226,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String resultText = tvResult.getText().toString();
         currentNum = new BigDecimal(resultText);
 
-        switch (btnText) {
-            case "AC":
+        switch (btn.getId()) {
+            case R.id.btnAC:
                 //If user has clicked any operation buttons -> dispose it
                 if (pressedBtnId != 0) {
-                    disposeState(findViewById(pressedBtnId));
+                    disposeBtnColor(findViewById(pressedBtnId));
                 }
                 needCalcState = false;
                 clickState = false;
                 newSolState = false;
                 finalResult = "0";
                 break;
-            case "+/-":
+            case R.id.btnPlus_or_Sub:
                 //Program only permits users to change a positive number to negative or backwards if they haven't clicked any operation buttons yet
-                if (!clickState) {
+                if (clickState) {
+                    currentNum = new BigDecimal("0").subtract(currentNum);
+                    finalResult = currentNum.stripTrailingZeros() + "";
+                    tempNum = currentNum;
+                } else {
                     currentNum = new BigDecimal("0").subtract(currentNum);
                     finalResult = currentNum.stripTrailingZeros() + "";
                 }
                 break;
-            case "%":
+            case R.id.btnPercent:
                 currentNum = currentNum.divide(new BigDecimal("100"));
                 finalResult = currentNum.stripTrailingZeros() + "";
                 break;
-            case "/":
-                pressAction(findViewById(R.id.btnDivide));
+            case R.id.btnDivide:
+                pressButton(findViewById(R.id.btnDivide));
                 break;
-            case "X":
-                pressAction(findViewById(R.id.btnMultiply));
+            case R.id.btnMultiply:
+                pressButton(findViewById(R.id.btnMultiply));
                 break;
-            case "-":
-                pressAction(findViewById(R.id.btnSubtract));
+            case R.id.btnSubtract:
+                pressButton(findViewById(R.id.btnSubtract));
                 break;
-            case "+":
-                pressAction(findViewById(R.id.btnPlus));
+            case R.id.btnPlus:
+                pressButton(findViewById(R.id.btnPlus));
                 break;
-            case "=":
+            case R.id.btnEqual:
                 calc();
                 break;
-            case ".":
+            case R.id.btnComma:
                 if (!resultText.contains(".")) {
                     finalResult += btnText;
                 }
                 break;
+            case R.id.btnHistory:
+                //Pass the array to history activity
+                Intent i = new Intent(getApplicationContext(), HistoryActivity.class);
+                i.putExtra("solList", solList);
+                startActivity(i);
+                break;
             default:
-                //If program is ready for new solution
+                //If program is ready for new solution -> Clear screen and add new number
                 if (newSolState) {
                     finalResult = btnText;
                     newSolState = false;
                 } else {
+                    //If the screen only has "0" then user cannot add more "0"
                     if (resultText.length() < 2 && resultText.indexOf("0") == 0) {
                         finalResult = btnText;
                     } else {
+                        //If a operation button has been clicked -> clear A number and show B number
                         if (clickState) {
                             finalResult = btnText;
                             clickState = false;
                             needCalcState = true;
-                        } else {
+                        } else if (finalResult.length() <= 8){
                             finalResult += btnText;
                         }
                     }
